6aafff4431cfeb32a73f98d7f46959539fe911f1
Kay Makowsky <kay.makowsky@hitabis.de>
2025-03-25 16:00:00 +0100

    remove compiled files

--- packages/profiler-jest-environment/constants.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const ENABLE_MEASUREMENTS: string | undefined;
--- packages/profiler-jest-environment/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENABLE_MEASUREMENTS = void 0;
-exports.ENABLE_MEASUREMENTS = process.env.ENABLE_MEASUREMENTS;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uc3RhbnRzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uc3RhbnRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFhLFFBQUEsbUJBQW1CLEdBQUcsT0FBTyxDQUFDLEdBQUcsQ0FBQyxtQkFBbUIsQ0FBQSJ9
