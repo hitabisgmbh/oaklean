ea50ce52d16690a7de3d132f0bfb1a99cc104325
Kay Makowsky <kay.makowsky@hitabis.de>
2025-04-24 11:32:43 +0200

    improve CallRelationTracker

--- packages/profiler-core/src/helper/InsertCPUProfileHelper/CallRelationTracker.ts
+++ packages/profiler-core/src/helper/InsertCPUProfileHelper/CallRelationTracker.ts
@@ -1,16 +1,37 @@
 import { CallIdentifier } from './CallIdentifier'
 
+type MapEntry = {
+	children: string[],
+	linkCountToChild: Map<string, number>
+}
+
 export class CallRelationTracker {
-	private _map: Map<string, string[]>
+	/**
+	 * Tracks the relations between function calls.
+	 * 
+	 * _map - Maps a function call to its child calls.
+	 * parentCallIdentifier -> {
+	 * 	children: [childCallIdentifier1, childCallIdentifier2, ...],
+	 * 	perChild: {
+	 * 		childCallIdentifier1 -> number, // number of links between parent and child
+	 * 		childCallIdentifier2 -> number,	// number of links between parent and child
+	 * 		...
+	 * }
+	 */
+	private _map: Map<string, MapEntry>
 	private _internMap: Map<string, boolean>
 	private _externMap: Map<string, boolean>
 	private _langInternalMap: Map<string, boolean>
 
 	constructor() {
-		this._map = new Map<string, string[]>()
+		this._map = new Map<string, MapEntry>()
 		this._internMap = new Map<string, boolean>()
 		this._externMap = new Map<string, boolean>()
 		this._langInternalMap = new Map<string, boolean>()
+	}
+
+	get map() {
+		return this._map
 	}
 
 	/**
@@ -55,11 +76,21 @@ export class CallRelationTracker {
 	 * @returns {boolean} true if the child was removed, false otherwise
 	 */
 	removeLastChildRecord(callIdentifier: CallIdentifier): boolean {
-		const childCalls = this._map.get(callIdentifier.toString())
-		if (childCalls === undefined) {
+		const mapEntry = this._map.get(callIdentifier.toString())
+		if (mapEntry === undefined || mapEntry.children.length === 0) {
 			return false
 		}
-		childCalls.pop() // remove last child from parent
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const lastChildIdentifier = mapEntry.children.pop()! // remove last child from parent
+		const numberOfLinks = mapEntry.linkCountToChild.get(lastChildIdentifier) || 0
+		if (numberOfLinks > 1) {
+			// reduce the number of links between parent and child
+			mapEntry.linkCountToChild.set(lastChildIdentifier, numberOfLinks - 1)
+		} else {
+			// no more links between parent and child, remove counter
+			mapEntry.linkCountToChild.delete(lastChildIdentifier)
+		}
+
 		return true
 	}
 
@@ -77,10 +108,37 @@ export class CallRelationTracker {
 	 * Gives the amount of recorded child calls for a function call.
 	 * 
 	 * @param {CallIdentifier} callIdentifier - The call identifier
-	 * @returns {boolean} true if the call has child calls recorded, false otherwise
+	 * @returns {number} the number of child calls
 	 */
-	getChildrenCount(callIdentifier: CallIdentifier) {
-		return this._map.get(callIdentifier.toString())?.length || 0
+	getNumberOfChildren(callIdentifier: CallIdentifier): number {
+		const mapEntry = this._map.get(callIdentifier.toString())
+		if (mapEntry === undefined) {
+			return 0
+		}
+		return mapEntry.children.length
+	}
+
+	/**
+	 * Gives the amount of recorded links between two function calls.
+	 * 
+	 * @param {CallIdentifier} parentCallIdentifier - the caller's call identifier
+	 * @param {CallIdentifier} childCallIdentifier - the callee's call identifier  
+	 * @returns {number} the number of links between the two calls
+	 */
+	getNumberOfLinksBetweenCalls(
+		parentCallIdentifier: CallIdentifier,
+		childCallIdentifier: CallIdentifier
+	): number {
+		const childCalls = this._map.get(parentCallIdentifier.toString())
+		if (childCalls === undefined) {
+			return 0
+		}
+		const childCallIdentifierString = childCallIdentifier.toString()
+		const numberOfLinks = childCalls.linkCountToChild.get(childCallIdentifierString)
+		if (numberOfLinks === undefined) {
+			return 0
+		}
+		return numberOfLinks
 	}
 
 	/**
@@ -109,7 +167,12 @@ export class CallRelationTracker {
 	) {
 		if (!this.isCallRecorded(callIdentifier)) {
 			const callIdentifierString = callIdentifier.toString()
-			this._map.set(callIdentifierString, [])
+			this._map.set(callIdentifierString, 
+				{
+					children: [],
+					linkCountToChild: new Map<string, number>()
+				}
+			)
 			switch (kind) {
 				case 'intern':
 					this._internMap.set(callIdentifierString, true)
@@ -140,15 +203,17 @@ export class CallRelationTracker {
 		const selfCallIdentifierString = self.toString()
 		const parentCallIdentifierString = parent.toString()
 
-		let previousChildCalls = this._map.get(parentCallIdentifierString)
-		let alreadyLinked = false
-		if (previousChildCalls === undefined) {
-			previousChildCalls = []
-			this._map.set(parentCallIdentifierString, previousChildCalls)
-		} else {
-			alreadyLinked = previousChildCalls.includes(selfCallIdentifierString)
+		let previousEntry = this._map.get(parentCallIdentifierString)
+		if (previousEntry === undefined) {
+			previousEntry = {
+				children: [],
+				linkCountToChild: new Map<string, number>()
+			}
+			this._map.set(parentCallIdentifierString, previousEntry)
 		}
-		previousChildCalls.push(selfCallIdentifierString)
-		return alreadyLinked
+		previousEntry.children.push(selfCallIdentifierString)
+		const numberOfExistingLinks = previousEntry.linkCountToChild.get(selfCallIdentifierString) || 0
+		previousEntry.linkCountToChild.set(selfCallIdentifierString, numberOfExistingLinks + 1)
+		return numberOfExistingLinks > 0
 	}
 }
