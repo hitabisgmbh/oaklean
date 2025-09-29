dd870782a322d47ee9fd4afb31b0f83db2681d96
Kay Makowsky <kay.makowsky@hitabis.de>
2025-09-29 12:33:21 +0200

    move execution details test assets into its own file

--- packages/profiler-core/tests/model/ProjectReport.test.ts
+++ packages/profiler-core/tests/model/ProjectReport.test.ts
@@ -1,5 +1,11 @@
 import * as fs from 'fs'
 
+// Test assets
+import {
+	EXAMPLE_SYSTEM_INFORMATION,
+	EXAMPLE_EXECUTION_DETAILS
+} from './assets/ProjectReport/ExecutionDetails'
+
 import { UnifiedPath } from '../../src/system/UnifiedPath'
 import { ProjectReport } from '../../src/model/ProjectReport'
 import {
@@ -14,19 +20,18 @@ import { UPDATE_TEST_REPORTS } from '../constants/env'
 import { PermissionHelper } from '../../src/helper/PermissionHelper'
 import { ExternalResourceHelper } from '../../src/helper/ExternalResourceHelper'
 import { LoggerHelper } from '../../src/helper/LoggerHelper'
+// Types
 import {
 	UnifiedPath_string,
 	UnifiedPathPart_string,
 	IProjectReport,
 	ProjectIdentifier_string,
 	ProjectReportOrigin,
-	IProjectReportExecutionDetails,
 	ISourceNodeMetaData,
 	SourceNodeMetaDataType,
 	SourceNodeIdentifier_string,
 	SourceNodeIdentifierPart_string,
 	NodeModuleIdentifier_string,
-	ISystemInformation,
 	GitHash_string,
 	SensorInterfaceType,
 	ISensorValues,
@@ -42,42 +47,6 @@ import {
 
 const CURRENT_DIR = new UnifiedPath(__dirname)
 const ROOT_DIR = CURRENT_DIR.join('..', '..', '..', '..')
-
-const EXAMPLE_SYSTEM_INFORMATION: ISystemInformation = JSON.parse(
-	fs.readFileSync(CURRENT_DIR.join('assets', 'SystemInformation', 'example.json').toString()).toString()
-) as ISystemInformation
-
-const EXAMPLE_EXECUTION_DETAILS = {
-	origin: ProjectReportOrigin.pure,
-	commitHash: '9828760b10d33c0fd06ed12cd6b6edf9fc4d6db0' as GitHash_string,
-	commitTimestamp: 1687845481077,
-	timestamp: 1687845481077,
-	highResolutionBeginTime: '887518894424000',
-	highResolutionStopTime: '887518904424000',
-	uncommittedChanges: false,
-	systemInformation: EXAMPLE_SYSTEM_INFORMATION,
-	languageInformation: {
-		name: 'node',
-		version: '20.11.1'
-	},
-	runTimeOptions: {
-		seeds: {
-			'Math.random': '0'
-		},
-		v8: {
-			cpu: {
-				sampleInterval: 1 as MicroSeconds_number
-			}
-		},
-		sensorInterface: {
-			type: SensorInterfaceType.powermetrics,
-			options: {
-				sampleInterval: 1000 as MicroSeconds_number,
-				outputFilePath: '<anonymized>'
-			}
-		}
-	}
-} satisfies IProjectReportExecutionDetails
 
 const EXAMPLE_PROJECT_REPORT: IProjectReport = {
 	reportVersion: VERSION,
